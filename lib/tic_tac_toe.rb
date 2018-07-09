9e34470a0df8ebaa029edb27980a88fbdbf48e51
Lindsay Braviak <lbraviak@gmail.com>
2018-07-09 19:14:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,7 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
+
 WIN_COMBINATIONS = [
   [0,1,2], [3,4,5],[6,7,8],[0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]
 ]
@@ -22,7 +22,7 @@ end
   def won?
     combo = []
     board.all? { |e| e == " " || e == ""}
-  
+
     if @board[0] == @board[1] && @board[0] === @board[2] && (@board[0] == "X" || @board[0] == "O")
       true
       combo = WIN_COMBINATIONS[0]
@@ -50,6 +50,6 @@ end
     else
       false
     end
-  
+
   end
 end
