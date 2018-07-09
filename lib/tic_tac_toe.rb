a9b8c302f7e797afc755b89f720367ef8eae5853
Lindsay Braviak <lbraviak@gmail.com>
2018-07-09 19:19:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,44 +11,15 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-WIN_COMBINATIONS = [
-  [0,1,2], [3,4,5],[6,7,8],[0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]
-]
+  WIN_COMBINATIONS = [
+    [0,1,2], [3,4,5],[6,7,8],[0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]
+  ]
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-  def won?
-    combo = []
-    board.all? { |e| e == " " || e == ""}
-
-    if @board[0] == @board[1] && @board[0] === @board[2] && (@board[0] == "X" || @board[0] == "O")
-      true
-      combo = WIN_COMBINATIONS[0]
-    elsif  @board[3] == @board[4] && @board[3] === @board[5] && (@board[5] == "X" || @board[5] == "O")
-      true
-      combo = WIN_COMBINATIONS[1]
-    elsif @board[6] == @board[7] && @board[6] === @board[8] && (@board[8] == "X" || @board[8] == "O")
-      true
-      combo = WIN_COMBINATIONS[2]
-    elsif @board[0] == @board[4] && @board[0] === @board[8] && (@board[8] == "X" || @board[8] == "O")
-      true
-      combo = WIN_COMBINATIONS[3]
-    elsif @board[2] == @board[4] && @board[2] === @board[6] && (@board[6] == "X" || @board[6] == "O")
-      true
-      combo = WIN_COMBINATIONS[4]
-    elsif  @board[0] == @board[3] && @board[0] === @board[6] && (@board[6] == "X" || @board[6] == "O")
-      true
-      combo =WIN_COMBINATIONS[5]
-    elsif  @board[1] == @board[4] && @board[1] === @board[7] && (@board[7] == "X" || @board[7] == "O")
-      true
-      combo = WIN_COMBINATIONS[6]
-    elsif  @board[2] == @board[5] && @board[2] === @board[8] && (@board[8] == "X" || @board[8] == "O")
-      true
-      combo = WIN_COMBINATIONS[7]
-    else
-      false
-    end
-
+  def input_to_index(user_input)
+    user_input.to_i - 1
   end
-end
+
+  def move(index, player)
+    player = current_player(@board)
+    @board[index] = player
+  end
